198d5b4674768fbceae0fa207c93303880c5e6f0
Brandon Espinoza <espinozab100@gmail.com>
2015-11-04 12:48:30 -0600

    Index.php with php includes and images

---
+++ index.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+		<!-- Basic page needs
+		============================================ -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Home 4 - Intensy</title>
+        <meta name="description" content="">
+
+		<!-- Mobile specific metas
+		============================================ -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Fonts
+		============================================ -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+
+		<!-- Favicon
+		============================================ -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+		<!-- CSS  -->
+
+		<!-- Bootstrap CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- font-awesome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- owl.carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+
+		<!-- animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+
+		<!-- fancybox CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/fancybox/jquery.fancybox.css">
+
+		<!-- meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+
+		<!-- RS slider CSS
+		============================================ -->
+		<link rel="stylesheet" type="text/css" href="lib/rs-plugin/css/settings.css" media="screen" />
+
+		<!-- normalize CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/normalize.css">
+
+		<!-- main CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/main.css">
+
+		<!-- style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/espinozabrand.css">
+
+		<!-- responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+
+		<!-- modernizr js
+		============================================ -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body class="home-4">
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+	<!-- header start -->
+	<?php include('SECTIONS/nav_home_page.php')?>
+	<!-- header end -->
+	<!-- HOME SLIDER -->
+	<?php include('SECTIONS/slider.php')?>
+	<!-- END HOME SLIDER -->
+	<!-- service-area start -->
+	<?php include('SECTIONS/services.php')?>
+	<!-- service-area end -->
+	<!-- features-area start -->
+	<?php include('SECTIONS/whats_new.php')?>
+	<!-- features-area end -->
+	<!-- upcoming-product-area start -->
+
+	<!-- upcoming-product-area start -->
+	<!-- counter-area start -->
+
+	<!-- counter-area end -->
+	<!-- category-area start -->
+
+	<!-- category-area end -->
+	<!-- video-area start -->
+
+	<!-- video-area end -->
+	<!-- free-shipping-area start -->
+
+	<!-- free-shipping-area end -->
+	<!-- popular-brand-area start -->
+
+	<!-- popular-brand-area end -->
+	<!-- latest-blog-area start -->
+
+	<!-- latest-blog-area end -->
+	<!-- footer start -->
+	<?php include('SECTIONS/footer.php')?>
+	<!-- footer end -->
+
+		<!-- JS -->
+
+		<!-- jquery js -->
+        <script src="js/vendor/jquery-1.11.2.min.js"></script>
+
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+
+		<!-- owl.carousel.min js -->
+        <script src="js/owl.carousel.min.js"></script>
+
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+
+		<!-- jquery.countdown js -->
+        <script src="js/jquery.countdown.min.js"></script>
+
+		<!-- parallax js -->
+        <script src="js/parallax.js"></script>
+
+		<!-- jquery.collapse js -->
+        <script src="js/jquery.collapse.js"></script>
+
+		<!-- jquery.easing js -->
+        <script src="js/jquery.easing.1.3.min.js"></script>
+
+		<!-- jquery.scrollUp js -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+
+		<!-- knob circle js -->
+        <script src="js/jquery.knob.js"></script>
+
+		<!-- jquery.appear js -->
+		<script src="js/jquery.appear.js"></script>
+
+		<!-- jquery.mixitup js -->
+		<script src="js/jquery.mixitup.min.js"></script>
+
+		<!-- fancybox js -->
+		<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+
+		<!-- jquery.counterup js -->
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+
+		<!-- rs-plugin js -->
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+		<script src="lib/rs-plugin/rs.home.js"></script>
+
+    <!-- SMOTH SCROLLING -->
+    <script type="text/javascript" src="js/smoth_scrolling.js"></script>
+
+
+		<!-- wow js -->
+        <script src="js/wow.js"></script>
+		<script>
+			new WOW().init();
+		</script>
+		<script>
+			var vid = document.getElementById("bg");
+			vid.loop = true;
+			vid.muted = true
+		</script>
+
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+
+		<!-- main js -->
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
