bc811b0a93eec2fd207ad86eacc121a26790f35f
Brandon Espinoza <espinozab100@gmail.com>
2015-11-04 12:52:52 -0600

    Contact page with form and location details

---
+++ contact-us.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+		<!-- Basic page needs
+		============================================ -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Contact us - Intensy</title>
+        <meta name="description" content="">
+
+		<!-- Mobile specific metas
+		============================================ -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Fonts
+		============================================ -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+
+		<!-- Favicon
+		============================================ -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+		<!-- CSS  -->
+
+		<!-- Bootstrap CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- font-awesome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- owl.carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+
+		<!-- animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+
+		<!-- fancybox CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/fancybox/jquery.fancybox.css">
+
+		<!-- meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+
+		<!-- RS slider CSS
+		============================================ -->
+		<link rel="stylesheet" type="text/css" href="lib/rs-plugin/css/settings.css" media="screen" />
+
+		<!-- normalize CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/normalize.css">
+
+		<!-- main CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/main.css">
+
+		<!-- style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+
+		<!-- responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+
+		<!-- modernizr js
+		============================================ -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+	<!-- header start -->
+	<?php include('SECTIONS/nav.php')?>
+	<!-- header end -->
+	<!-- breadcrumb-area start -->
+	<div class="breadcrumb-area contact-pages-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 breadcrumb-padding">
+					<div class="breadcrumb-title">
+						<h1 style="color: #00a3ff">Contact Us</h1>
+						<p style="color: #00a3ff">Great Customer Service</p>
+					</div>
+					<div class="breadcrumb-list">
+						<ul class="breadcrumb">
+							<li>You are here: </li>
+							<li><a href="index.php">Home</a></li>
+							<li>Contact Us Page</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- breadcrumb-area end -->
+	<!-- contact-page-area start -->
+
+	<div class="container">
+  	<div class="row" style="padding-top: 100px;">
+    	<div class="col-md-4">
+      	<div class="contact-content">
+					<div class="contact-address">
+						<p>Get in Touch with Us</p>
+						<ul class="footer-top-contact">
+							<li>
+								<span class="contact-icon"><i class="fa fa-home"></i></span>
+								<span class="contact-text">818 Union Street, Shelbyville, TN 37160</span>
+							</li>
+							<li>
+								<span class="contact-icon"><i class="fa fa-phone-square"></i></span>
+								<span class="contact-text">(931) 607 1051 </span>
+							</li>
+						</ul>
+					</div>
+      	</div>
+    	</div>
+    	<div class="col-md-4">
+      	<div class="contact-map">
+					<h3>We are located</h3>
+					<div id="googleMap" style="width:100%;height:430px;"></div>
+				</div>
+    	</div>
+    	<div class="col-md-4">
+      	<div class="contact-content">
+					<div class="contact-form-ac">
+						<p>Send an email. All fields with an * are required.</p>
+						<form action="#">
+							<div class="row">
+								<div class="col-md-4 col-sm-4">
+									<div class="control-level">
+										<p><span>Name</span> *</p>
+										<input type="text" />
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="control-level">
+										<p><span>Email</span> *</p>
+										<input type="email" />
+									</div>
+								</div>
+								<div class="col-md-4 col-sm-4">
+									<div class="control-level">
+										<p><span>Subject</span> *</p>
+										<input type="text" />
+									</div>
+								</div>
+								<div class="col-md-12 col-sm-12">
+									<div class="textarea-contact">
+										<p><span>Message</span> *</p>
+										<textarea id="message-ac" cols="30" rows="10"></textarea>
+									</div>
+								</div>
+								<div class="col-md-12 col-sm-12">
+									<div class="contact_email_copy">
+										<input type="checkbox" />
+										<p>Send copy to yourself</p>
+									</div>
+								</div>
+								<div class="col-md-12 col-sm-12">
+									<div class="send-mail">
+										<input style="border-color: #00a3ff " class="readon" type="button" value="Send Email"/>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+    	</div>
+  	</div>
+	</div>
+	<!-- contact-page-area end -->
+
+	<!-- footer start -->
+	<?php include('SECTIONS/footer.php')?>
+	<!-- footer end -->
+
+		<!-- JS -->
+
+		<!-- jquery js -->
+        <script src="js/vendor/jquery-1.11.2.min.js"></script>
+
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+
+		<!-- owl.carousel.min js -->
+        <script src="js/owl.carousel.min.js"></script>
+
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+
+		<!-- jquery.countdown js -->
+        <script src="js/jquery.countdown.min.js"></script>
+
+		<!-- parallax js -->
+        <script src="js/parallax.js"></script>
+
+		<!-- jquery.collapse js -->
+        <script src="js/jquery.collapse.js"></script>
+
+		<!-- jquery.easing js -->
+        <script src="js/jquery.easing.1.3.min.js"></script>
+
+		<!-- jquery.scrollUp js -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+
+		<!-- knob circle js -->
+        <script src="js/jquery.knob.js"></script>
+
+		<!-- jquery.appear js -->
+		<script src="js/jquery.appear.js"></script>
+
+		<!-- jquery.mixitup js -->
+		<script src="js/jquery.mixitup.min.js"></script>
+
+		<!-- fancybox js -->
+		<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+
+		<!-- jquery.counterup js -->
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+
+		<!-- rs-plugin js -->
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+		<script src="lib/rs-plugin/rs.home.js"></script>
+
+		<!-- Google Map js -->
+        <script src="https://maps.googleapis.com/maps/api/js"></script>
+		<script>
+			function initialize() {
+			  var mapOptions = {
+				zoom: 12,
+				scrollwheel: false,
+				center: new google.maps.LatLng(35.4930963,-86.4715117)
+			  };
+
+			  var map = new google.maps.Map(document.getElementById('googleMap'),
+				  mapOptions);
+
+
+			  var marker = new google.maps.Marker({
+				position: map.getCenter(),
+				animation:google.maps.Animation.BOUNCE,
+				icon: 'img/map-marker1.png',
+				map: map
+			  });
+
+			}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
+		</script>
+
+		<!-- wow js -->
+        <script src="js/wow.js"></script>
+		<script>
+			new WOW().init();
+		</script>
+
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+
+		<!-- main js -->
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
