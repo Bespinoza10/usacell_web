daa25ea140fb673f6d054447306cfd350178a8c7
Brandon Espinoza <espinozab100@gmail.com>
2015-11-04 12:50:50 -0600

    Shopping Page with items

---
+++ shop.php
@@ -0,0 +1,488 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+		<!-- Basic page needs
+		============================================ -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Shop - Intensy</title>
+        <meta name="description" content="">
+
+		<!-- Mobile specific metas
+		============================================ -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Fonts
+		============================================ -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+
+		<!-- Favicon
+		============================================ -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+		<!-- CSS  -->
+
+		<!-- Bootstrap CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- font-awesome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- owl.carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+
+		<!-- animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+
+		<!-- fancybox CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/fancybox/jquery.fancybox.css">
+
+		<!-- meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+
+		<!-- RS slider CSS
+		============================================ -->
+		<link rel="stylesheet" type="text/css" href="lib/rs-plugin/css/settings.css" media="screen" />
+
+		<!-- normalize CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/normalize.css">
+
+		<!-- main CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/main.css">
+
+		<!-- style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+
+		<!-- responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/espinozabrand.css">
+
+		<!-- modernizr js
+		============================================ -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+	<!-- header start -->
+	<?php include('SECTIONS/nav.php')?>
+	<!-- header end -->
+	<!-- breadcrumb-area start -->
+	<div class="breadcrumb-area pages-area-shop">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 breadcrumb-padding pages-p">
+					<div class="breadcrumb-title">
+						<h1>Shop For The <br><br> <font color="#FF963D">Latest Devices</font></h1>
+					</div>
+					<div class="breadcrumb-list">
+						<ul class="breadcrumb">
+							<li>You are here: </li>
+							<li><a href="index.php">Home</a></li>
+							<li>Shop</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- breadcrumb-area end -->
+	<!-- shop-area start-->
+	<div class="shop-area">
+		<div class="container">
+			<div class="row">
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/1.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/2.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/3.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/4.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/5.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/6.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/7.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/8.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/9.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/10.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/11.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+				<!-- single-shop start -->
+				<div class="col-lg-3 col-md-3 col-sm-4">
+					<div class="single-features single-shop">
+						<div class="features-img shop-img">
+							<a href="#"><img src="img/shop/12.jpg" alt="" /></a>
+							<span>NEW</span>
+
+						</div>
+						<div class="features-info">
+							<h2><a href="#">Iphone 1</a></h2>
+							<p><a href="#">WC-AB-001</a></p>
+							<h3><span>$58.00</span> each</h3>
+							<div class="fea-rating">
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<a href="#"><i class="fa fa-star"></i></a>
+								<span>(7)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- single-shop end -->
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<div class="shop-pagination">
+						<div class="pagination-shop">
+							<ul>
+								<li class="display">Display #</li>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">Next</a></li>
+								<li><a href="#">End</a></li>
+								<li class="page-one"> Page 1 of 2</li>
+							</ul>
+						</div>
+						<div class="page-right">
+							<p>Results 1 - 12 of 16</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- shop-area end-->
+
+	<!-- footer start -->
+	<?php include('SECTIONS/footer.php')?>
+	<!-- footer end -->
+
+		<!-- JS -->
+
+		<!-- jquery js -->
+        <script src="js/vendor/jquery-1.11.2.min.js"></script>
+
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+
+		<!-- owl.carousel.min js -->
+        <script src="js/owl.carousel.min.js"></script>
+
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+
+		<!-- jquery.countdown js -->
+        <script src="js/jquery.countdown.min.js"></script>
+
+		<!-- parallax js -->
+        <script src="js/parallax.js"></script>
+
+		<!-- jquery.collapse js -->
+        <script src="js/jquery.collapse.js"></script>
+
+		<!-- jquery.easing js -->
+        <script src="js/jquery.easing.1.3.min.js"></script>
+
+		<!-- jquery.scrollUp js -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+
+		<!-- knob circle js -->
+        <script src="js/jquery.knob.js"></script>
+
+		<!-- jquery.appear js -->
+		<script src="js/jquery.appear.js"></script>
+
+		<!-- jquery.mixitup js -->
+		<script src="js/jquery.mixitup.min.js"></script>
+
+		<!-- fancybox js -->
+		<script src="js/fancybox/jquery.fancybox.pack.js"></script>
+
+		<!-- jquery.counterup js -->
+        <script src="js/jquery.counterup.min.js"></script>
+        <script src="js/waypoints.min.js"></script>
+
+		<!-- rs-plugin js -->
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+		<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+		<script src="lib/rs-plugin/rs.home.js"></script>
+
+		<!-- wow js -->
+        <script src="js/wow.js"></script>
+		<script>
+			new WOW().init();
+		</script>
+
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+
+		<!-- main js -->
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
